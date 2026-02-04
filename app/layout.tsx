65e2cd87cb69283a8fba1d6478687b433f86eed1
Jerwin Manongsong <61395841+nahadgur@users.noreply.github.com>
2026-02-04 19:32:47 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+    name="google-site-verification"
+    content="RTGtfmUJlOBad0COgiVkh4euoW5CO9sQCgkDwmnj_dM"
+  />
+        <link rel="icon" href="/favicon.ico" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-BVFXE6F28R"
